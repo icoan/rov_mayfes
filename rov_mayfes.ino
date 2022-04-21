ce86d39fe872cea9583b08a2dec5f52bde1d9271
icoan <naokikaneko777@gmail.com>
2022-04-22 00:28:58 +0900

    スラスター出力初期化後の待機を追加。コードサンプルのコメントをりどみに移動。その他整理。
    Signed-off-by: icoan <naokikaneko777@gmail.com>

--- rov_mayfes.ino
+++ rov_mayfes.ino
@@ -9,26 +9,18 @@
 #include <WiFiClient.h>
 #include <BlynkSimpleEsp32.h>
 #include <ESP32Servo.h>
-#include "config.h"
+#include "config.h" //wifiパスワードなんかを外部ファイルから読み込む際に使用
 
 //ESP32の注意点として、書き込み時にBootボタンを押さないとコンパイルエラーが出ることがある
-//
-//参考
-//Example集: https://examples.blynk.cc/?board=ESP32&shield=ESP32%20WiFi&example=GettingStarted%2FVirtualPinWrite
-//ESP32のPWM: https://randomnerdtutorials.com/esp32-pwm-arduino-ide/
-//ESP32のPWM: https://www.mgo-tec.com/blog-entry-ledc-pwm-arduino-esp32.html#title02
 
-//トークン発行して入れてください
 char auth[] = BLYNK_AUTH_TOKEN;
-
-// Your WiFi credentials.
-// Set password to "" for open networks.
 char ssid[] = WIFI_SSID;
 char pass[] = WIFI_PASSWORD;
 
 Servo servo1;
 Servo servo2;
 Servo servo3;
+
 const int maxUs = 1900;
 const int minUs = 1100;
 const int servo1Pin = 19;//表記はGPIO12
@@ -37,6 +29,7 @@ const int servo2Pin = 18;//表記は13
 const int servo2Period = 50;
 const int servo3Pin = 23;//表記は13
 const int servo3Period = 50;
+
 int servo1Us = 1500;
 int servo2Us = 1500;
 int servo3Us = 1500;
@@ -44,9 +37,7 @@ int servo3Us = 1500;
 int turnClockAmount = 0;
 int forwardAmount = 0;
 float turnStrength = 0.5;
-float forwardStrength = -0.7;
-
-
+float forwardStrength = 0.7;
 
 //アプリ側でVirtual Pinに書き込みがあるたびに呼ばれる関数
 //paramがV6に書き込まれたデータで、asInt()でInt型として処理 asFloatとかも色々ある
@@ -58,9 +49,7 @@ float forwardStrength = -0.7;
 
 void myTimerEvent()
 {
-  // You can send any value at any time.
-  // Please don't send more that 10 values per second.
-  Blynk.virtualWrite(V5, millis() / 1000);
+  Blynk.virtualWrite(V5, millis() / 1000);//起動してからの経過時間
 }
 
 BLYNK_WRITE(V8)
@@ -73,19 +62,20 @@ BLYNK_WRITE(V9)
   forwardAmount = param.asInt();
 }
 
-
 BlynkTimer timer1;
 BlynkTimer timer2;
 
 
-int curve1(int x)
+int curve1(int input)
 {
-  return 1500 + round(turnStrength*x);
+  int x = 1500 + round(turnStrength * input);
+  return constrain(x, minUs, maxUs);
 }
 
-int curve2(int x)
+int curve2(int input)
 {
-  return 1500 - round(forwardStrength*x);
+  int x = 1500 + round(forwardStrength * input);
+  return constrain(x, minUs, maxUs);
 }
 
 void servoLoop()
@@ -93,6 +83,7 @@ void servoLoop()
   servo1Us = curve1(turnClockAmount);
   servo2Us = curve1(-turnClockAmount);
   servo3Us = curve2(forwardAmount);
+
   servo1.writeMicroseconds(servo1Us);
   servo2.writeMicroseconds(servo2Us);
   servo3.writeMicroseconds(servo3Us);
@@ -103,16 +94,16 @@ void setup()
   delay(100);
   // Debug console
   Serial.begin(9600);
+
   ESP32PWM::allocateTimer(0);
   ESP32PWM::allocateTimer(1);
   ESP32PWM::allocateTimer(2);
   ESP32PWM::allocateTimer(3);
+
   servo1.setPeriodHertz(servo1Period);
   servo1.attach(servo1Pin, minUs, maxUs);
-
   servo2.setPeriodHertz(servo2Period);
   servo2.attach(servo2Pin, minUs, maxUs);
-
   servo3.setPeriodHertz(servo3Period);
   servo3.attach(servo3Pin, minUs, maxUs);
 
@@ -121,6 +112,9 @@ void setup()
   servo1.writeMicroseconds(1500);
   servo2.writeMicroseconds(1500);
   servo3.writeMicroseconds(1500);
+
+  delay(3000);
+
   Blynk.begin(auth, ssid, pass);
 
   timer1.setInterval(1000L, myTimerEvent);
@@ -130,6 +124,6 @@ void setup()
 void loop()
 {
   Blynk.run();
-   timer1.run(); // Initiates BlynkTimer
+  timer1.run();
   timer2.run();
 }
