75cea2762ff0ed19fdd6a03cfcd5f27e8423b273
icoan <naokikaneko777@gmail.com>
2022-05-14 18:51:09 +0900

    サーボに制御信号を送る関数を入れ替え。サーボ２が中央だった。

--- rov_mayfes.ino
+++ rov_mayfes.ino
@@ -1,8 +1,8 @@
 #define BLYNK_TEMPLATE_ID "TMPLjvg-o1rH"
 #define BLYNK_DEVICE_NAME "Red"
 #define BLYNK_AUTH_TOKEN "XWgLBdtNfMilZn8TsdO0RgVIAzhL8XUU"
-#define WIFI_SSID "Xperia YHf41b"
-#define WIFI_PASSWORD "f49a25e5a2f7q"
+#define WIFI_SSID "AiR-WiFi_0OT1A1"
+#define WIFI_PASSWORD "80680466"
 #define BLYNK_PRINT Serial
 
 #define SERIAL_PRINT_ON//いらなきゃCOすること
@@ -23,6 +23,7 @@ Servo servo1;
 Servo servo2;
 Servo servo3;
 
+const int neutralUs = 1500;
 const int maxUs = 1900;
 const int minUs = 1100;
 
@@ -33,14 +34,14 @@ const int servo2Period = 50;
 const int servo3Pin = 27;//表記は13
 const int servo3Period = 50;
 
-int servo1Us = 1500;
-int servo2Us = 1500;
-int servo3Us = 1500;
+int servo1Us = neutralUs;
+int servo2Us = neutralUs;
+int servo3Us = neutralUs;
 
-int accelAmount = 1;
+float accelAmount = 1.0f;
 int turnClockAmount = 0;
 int forwardAmount = 0;
-float turnStrength = 0.5;
+float turnStrength = 0.7;
 float forwardStrength = 0.7;
 
 //アプリ側でVirtual Pinに書き込みがあるたびに呼ばれる関数
@@ -48,99 +49,99 @@ float forwardStrength = 0.7;
 
 void myTimerEvent()
 {
-  Blynk.virtualWrite(V5, millis() / 1000);//起動してからの経過時間
+  	Blynk.virtualWrite(V5, millis() / 1000);//起動してからの経過時間
 }
 
 BLYNK_WRITE(V7)
 {
-  accelAmount = param.asDouble();
+  	accelAmount = param.asInt() / 100.0f;
 }
 
 BLYNK_WRITE(V8)
 {
-  turnClockAmount = param.asInt();
+  	turnClockAmount = param.asInt();
 }
 
 BLYNK_WRITE(V9)
 {
-  forwardAmount = param.asInt();
+  	forwardAmount = -param.asInt();
 }
 
-BlynkTimer timer1;
-BlynkTimer timer2;
+BlynkTimer dataSendTimer;
+BlynkTimer controlLogicTimer;
 
 
-int curve1(int input, double accel)
+int curve1(int input)
 {
-  int x = 1500 + round(turnStrength * input * accel);
-  return constrain(x, minUs, maxUs);
+    int x = neutralUs + round(turnStrength * input * accelAmount);
+    return constrain(x, minUs, maxUs);
 }
 
-int curve2(int input, double accel)
+int curve2(int input)
 {
-  int x = 1500 + round(forwardStrength * input * accel);
-  return constrain(x, minUs, maxUs);
+    int x = neutralUs + round(forwardStrength * input * accelAmount);
+    return constrain(x, minUs, maxUs);
 }
 
 void servoLoop()
 {
-  servo1Us = curve1(turnClockAmount, accelAmount);
-  servo2Us = curve1(-turnClockAmount, accelAmount);
-  servo3Us = curve2(forwardAmount, accelAmount);
+    servo1Us = curve1(turnClockAmount);
+    servo2Us = curve2(-forwardAmount);
+    servo3Us = curve1(turnClockAmount);
 
-  servo1.writeMicroseconds(servo1Us);
-  servo2.writeMicroseconds(servo2Us);
-  servo3.writeMicroseconds(servo3Us);
+    servo1.writeMicroseconds(servo1Us);
+    servo2.writeMicroseconds(servo2Us);
+    servo3.writeMicroseconds(servo3Us);
 
-  #ifdef SERIAL_PRINT_ON
-    Serial.print(servo1Us);
-    Serial.print(", ");
-    Serial.print(servo2Us);
-    Serial.print(", ");
-    Serial.println(servo3Us);
-  #endif
+    #ifdef SERIAL_PRINT_ON
+        Serial.print(servo1Us - neutralUs);
+        Serial.print(", ");
+        Serial.print(servo2Us - neutralUs);
+        Serial.print(", ");
+        Serial.println(servo3Us - neutralUs);
+    #endif
 } 
 
 void setup()
 {
-  delay(100);
-  // Debug console
-  Serial.begin(9600);
+    delay(100);
+    // Debug console
+    Serial.begin(9600);
 
-  ESP32PWM::allocateTimer(0);
-  ESP32PWM::allocateTimer(1);
-  ESP32PWM::allocateTimer(2);
-  ESP32PWM::allocateTimer(3);
+    ESP32PWM::allocateTimer(0);
+    ESP32PWM::allocateTimer(1);
+    ESP32PWM::allocateTimer(2);
+    ESP32PWM::allocateTimer(3);
 
-  servo1.setPeriodHertz(servo1Period);
-  servo1.attach(servo1Pin, minUs, maxUs);
-  servo2.setPeriodHertz(servo2Period);
-  servo2.attach(servo2Pin, minUs, maxUs);
-  servo3.setPeriodHertz(servo3Period);
-  servo3.attach(servo3Pin, minUs, maxUs);
+    servo1.setPeriodHertz(servo1Period);
+    servo1.attach(servo1Pin, minUs, maxUs);
+    servo2.setPeriodHertz(servo2Period);
+    servo2.attach(servo2Pin, minUs, maxUs);
+    servo3.setPeriodHertz(servo3Period);
+    servo3.attach(servo3Pin, minUs, maxUs);
 
-  Serial.println("Servo Attached");
+    Serial.println("Servo Attached");
 
-  delay(1000);
+    delay(1000);
 
-  servo1.writeMicroseconds(1500);
-  servo2.writeMicroseconds(1500);
-  servo3.writeMicroseconds(1500);
+    servo1.writeMicroseconds(1500);
+    servo2.writeMicroseconds(1500);
+    servo3.writeMicroseconds(1500);
 
-  Serial.println("Servo Ready");
+    Serial.println("Servo Ready");
 
-  delay(3000);
+    delay(3000);
 
-  Blynk.begin(auth, ssid, pass);
+    Blynk.begin(auth, ssid, pass);
 
-  timer1.setInterval(1000L, myTimerEvent);
-  timer2.setInterval(20L, servoLoop);
+    dataSendTimer.setInterval(1000L, myTimerEvent);
+    controlLogicTimer.setInterval(20L, servoLoop);
 }
 
 void loop()
 {
-  Serial.println("Running Now");
-  Blynk.run();
-  timer1.run();
-  timer2.run();
+    Serial.println("Running Now");
+    Blynk.run();
+    dataSendTimer.run();
+    controlLogicTimer.run();
 }
