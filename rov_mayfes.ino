c8e8a46c0e51e4c04b640dc7261cbd37e0552a48
icoan <naokikaneko777@gmail.com>
2023-02-22 15:58:09 +0900

    実験用に書き換え

--- rov_mayfes.ino
+++ rov_mayfes.ino
@@ -1,11 +1,12 @@
 #define BLYNK_TEMPLATE_ID "TMPLjvg-o1rH"
-#define BLYNK_DEVICE_NAME "Red"
-#define BLYNK_AUTH_TOKEN "XWgLBdtNfMilZn8TsdO0RgVIAzhL8XUU"
-#define WIFI_SSID "AiR-WiFi_0OT1A1"
-#define WIFI_PASSWORD "80680466"
+#define BLYNK_DEVICE_NAME "White"                           // Red or White
+#define BLYNK_AUTH_TOKEN "XWgLBdtNfMilZn8TsdO0RgVIAzhL8XUU" // White
+// #define BLYNK_AUTH_TOKEN "YKL7W_XpqUu_MIYGKqP1IrevpxR_eVgT" // Red
+#define WIFI_SSID "Galaxy_5GMW_7641"
+#define WIFI_PASSWORD "uvum8745"
 #define BLYNK_PRINT Serial
 
-#define SERIAL_PRINT_ON//いらなきゃCOすること
+#define SERIAL_PRINT_ON // いらなきゃCOすること
 
 #include <WiFi.h>
 #include <WiFiClient.h>
@@ -13,7 +14,7 @@
 #include <ESP32Servo.h>
 #include "config.h" //wifiパスワードなんかを外部ファイルから読み込む際に使用
 
-//ESP32の注意点として、書き込み時にBootボタンを押さないとコンパイルエラーが出ることがある
+// ESP32の注意点として、書き込み時にBootボタンを押さないとコンパイルエラーが出ることがある
 
 char auth[] = BLYNK_AUTH_TOKEN;
 char ssid[] = WIFI_SSID;
@@ -27,80 +28,92 @@ const int neutralUs = 1500;
 const int maxUs = 1900;
 const int minUs = 1100;
 
-const int servo1Pin = 25;//表記はGPIO12
+const int servo1Pin = 25; // 表記はGPIO12
 const int servo1Period = 50;
-const int servo2Pin = 26;//表記は13
+const int servo2Pin = 26; // 表記は13
 const int servo2Period = 50;
-const int servo3Pin = 27;//表記は13
+const int servo3Pin = 27; // 表記は13
 const int servo3Period = 50;
 
 int servo1Us = neutralUs;
 int servo2Us = neutralUs;
 int servo3Us = neutralUs;
 
-float accelAmount = 1.0f;
-int turnClockAmount = 0;
-int forwardAmount = 0;
-float turnStrength = 0.7;
-float forwardStrength = 0.7;
+float accelCoeff = 1.0f;
+int servoInput1 = 0;
+int servoInput2 = 0;
+int servoInput3 = 0;
+float Strength = 0.7;
 
-//アプリ側でVirtual Pinに書き込みがあるたびに呼ばれる関数
-//paramがV6に書き込まれたデータで、asInt()でInt型として処理 asFloatとかも色々ある
+// アプリ側でVirtual Pinに書き込みがあるたびに呼ばれる関数
+// paramがV6に書き込まれたデータで、asInt()でInt型として処理 asFloatとかも色々ある
 
 void myTimerEvent()
 {
-  	Blynk.virtualWrite(V5, millis() / 1000);//起動してからの経過時間
+    Blynk.virtualWrite(V5, millis() / 1000); // 起動してからの経過時間
 }
 
 BLYNK_WRITE(V7)
 {
-  	accelAmount = param.asInt() / 100.0f;
+    accelCoeff = param.asInt() / 100.0f;
 }
 
-BLYNK_WRITE(V8)
+BLYNK_WRITE(V1)
 {
-  	turnClockAmount = param.asInt();
+    servoInput1 = param.asInt();
 }
 
-BLYNK_WRITE(V9)
+BLYNK_WRITE(V2)
 {
-  	forwardAmount = -param.asInt();
+    servoInput2 = param.asInt();
+}
+
+BLYNK_WRITE(V3)
+{
+    servoInput3 = param.asInt();
 }
 
 BlynkTimer dataSendTimer;
 BlynkTimer controlLogicTimer;
 
-
+// スラスターの回転方向に合わせて＋round～か-round～に書き換え
 int curve1(int input)
 {
-    int x = neutralUs + round(turnStrength * input * accelAmount);
+    int x = neutralUs + round(input * accelCoeff);
     return constrain(x, minUs, maxUs);
 }
 
 int curve2(int input)
 {
-    int x = neutralUs + round(forwardStrength * input * accelAmount);
+    int x = neutralUs + round(input * accelCoeff);
+    return constrain(x, minUs, maxUs);
+}
+
+int curve3(int input)
+{
+    int x = neutralUs + round(input * accelCoeff);
     return constrain(x, minUs, maxUs);
 }
 
 void servoLoop()
 {
-    servo1Us = curve1(turnClockAmount);
-    servo2Us = curve2(-forwardAmount);
-    servo3Us = curve1(turnClockAmount);
+    servo1Us = curve1(servoInput1);
+    servo2Us = curve2(servoInput2);
+    servo3Us = curve3(servoInput3);
 
     servo1.writeMicroseconds(servo1Us);
     servo2.writeMicroseconds(servo2Us);
     servo3.writeMicroseconds(servo3Us);
 
-    #ifdef SERIAL_PRINT_ON
-        Serial.print(servo1Us - neutralUs);
-        Serial.print(", ");
-        Serial.print(servo2Us - neutralUs);
-        Serial.print(", ");
-        Serial.println(servo3Us - neutralUs);
-    #endif
-} 
+#ifdef SERIAL_PRINT_ON
+    Serial.print("Servo1: ");
+    Serial.print(servo1Us - neutralUs);
+    Serial.print(", Servo2: ");
+    Serial.print(servo2Us - neutralUs);
+    Serial.print(", Servo3: ");
+    Serial.println(servo3Us - neutralUs);
+#endif
+}
 
 void setup()
 {
@@ -124,7 +137,7 @@ void setup()
 
     delay(1000);
 
-    servo1.writeMicroseconds(1500);
+    servo1.writeMicroseconds(1600);
     servo2.writeMicroseconds(1500);
     servo3.writeMicroseconds(1500);
 
@@ -133,7 +146,6 @@ void setup()
     delay(3000);
 
     Blynk.begin(auth, ssid, pass);
-
     dataSendTimer.setInterval(1000L, myTimerEvent);
     controlLogicTimer.setInterval(20L, servoLoop);
 }
